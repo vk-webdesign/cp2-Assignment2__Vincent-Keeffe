ae922460747c95abcfcfac46090c9720d986c853
vkeeffe <vkeeffe@cca.edu>
2019-10-14 15:07:53 -0700

    Changes

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+const styles = {
+  transition: 'all 1s ease-in'
+};
+
 class NamePage extends Component {
   render() {
     return (
       <div className="page">
-        <p className="nameStory">
-          Greetings, traveler! What is your name?
-          <br />
-          <input
-            type="text"
-            className="name"
-            placeholder="Name"
-            value={this.props.data.name}
-            onChange={event =>
-              this.props.setStateFunction("name", event.target.value)
-            }
-          />
+        <span role="img" aria-label="space"> </span>
+        <div className= "play-area">
+        <p className="story nameStory">
+          Greetings, traveler! What is your name?      
+          <img className="img-name" src="" />
+            <input
+              type="text"
+              className="name"
+              placeholder="Name"
+              value={this.props.data.name}
+              onChange={event =>
+                this.props.setStateFunction("name", event.target.value)
+              }
+            />      
         </p>
         <div className="btn-area">
-          <div className="btn-skin">
           <button onClick={() => this.props.goFunction(StartPage)}>
             Continue...
           </button>
@@ -35,16 +40,19 @@ class StartPage extends Component {
   render() {
     return (
       <div className="page">
+        <div className="play-area">
         <p className="story">
-          Welcome, {this.props.data.name}! How would you like to get to your
-          destination
+          Welcome, {this.props.data.name}! You've been accepted into Evian Industries, congratulations! 
         </p>
         <div className="btn-area">
-          <button className="btn1" onClick={() => this.props.goFunction(TrainPage)}>
-            Train
+          <button onClick={() => this.props.goFunction(TrainPage)}>
+            What is Evian Industries?
           </button>
-          <button className="btn1" onClick={() => this.props.goFunction(ShipPage)}>Ship</button>
-        </div>
+          <button onClick={() => this.props.goFunction(ShipPage)}>
+            Okay…
+            </button>
+          </div>
+          </div>
       </div>
     );
   }
@@ -54,25 +62,91 @@ class TrainPage extends Component {
   render() {
     return (
       <div className="page">
+        <div className="play-area">
         <p className="story">
-          Welcome aboard the choo-choo train! Please make your way to your seat.
-          What is the number?
+          Evian Industries is the largest industrial corps. in this sector of the galaxy.
         </p>
-        <select
-          value={this.props.data.seat}
-          onChange={event =>
-            this.props.setStateFunction("seat", event.target.value)
-          }
-        >
-          <option>12</option>
-          <option>24</option>
-          <option>63</option>
-          <option>53</option>
-        </select>
-        <div className="btn-area">
+        
+          <div className="btn-area">
+            <button onClick={() => this.props.goFunction(willI)}>
+              What will I be doing for Evian Industries?
+            </button>
+            <button onClick={() => this.props.goFunction(galaxy)}>
+              Did you say sector of the galaxy?
+            </button>
           <button onClick={() => this.props.goFunction(travelPage)}>
             Continue...
           </button>
+          </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+class galaxy extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="play-area">
+          <p className="story">
+            That's right, we own 66% of all of the Mid-Eastern parts of the Milky Way.
+        </p>
+
+          <div className="btn-area">
+            <button onClick={() => this.props.goFunction(noChoices)}>
+              So I don't get to choose?
+            </button>
+            <button onClick={() => this.props.goFunction(travelPage)}>
+              Fine, continue...
+          </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class willI extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="play-area">
+          <p className="story">
+            Evian Industries processes all of our recruits and places them in the most appropriate positions.
+        </p>
+
+          <div className="btn-area">
+            <button onClick={() => this.props.goFunction(noChoices)}>
+              So I don't get to choose?
+            </button>
+            <button onClick={() => this.props.goFunction(travelPage)}>
+              Fine, continue...
+          </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class noChoices extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="play-area">
+          <p className="story">
+            Evian Industries processes all of our recruits and places them in the most appropriate positions.
+        </p>
+
+          <div className="btn-area">
+            <button onClick={() => this.props.goFunction(noChoices)}>
+              So I don't get to choose?
+            </button>
+            <button onClick={() => this.props.goFunction(travelPage)}>
+              Fine, continue...
+          </button>
+          </div>
         </div>
       </div>
     );
@@ -83,9 +157,9 @@ class ShipPage extends Component {
   render() {
     return (
       <div className="page">
+        <div className="play-area">
         <p className="story">
-          Welcome aboard the choo-choo train! Please make your way to your seat.
-          What is the number?
+          Now tell me a few things:
         </p>
         <select
           value={this.props.data.seat}
@@ -102,7 +176,8 @@ class ShipPage extends Component {
           <button onClick={() => this.props.goFunction(travelPage)}>
             Continue...
           </button>
-        </div>
+          </div>
+          </div>
       </div>
     );
   }
@@ -112,13 +187,15 @@ class travelPage extends Component {
   render() {
     return (
       <div className="page">
+        <div className="play-area">
         <p className="story">
           Ah, seat {this.props.data.seat}, excelent choice. Right this way.
         </p>
         <div className="btn-area">
           <button onClick={() => this.props.goFunction(walkPage)}>Walk</button>
           <button onClick={() => this.props.goFunction(walkPage)}>Run</button>
-        </div>
+          </div>
+          </div>
       </div>
     );
   }
@@ -129,6 +206,7 @@ class walkPage extends Component {
     if (this.props.data.seat % 2 === 0) {
       return (
         <div className="page">
+          <div className="play-area">
           <p className="story">
             Looking to the right for {this.props.data.seat}. Your walking
             continues and eventually you stumble upon a booth with a plaque
@@ -138,12 +216,14 @@ class walkPage extends Component {
             <button onClick={() => this.props.goFunction(seatPage)}>
               Sit down
             </button>
-          </div>
+            </div>
+            </div>
         </div>
       );
     } else {
       return (
         <div className="page">
+          <div className="play-area">
           <p className="story">
             Looking to the left for {this.props.data.seat}. Your walking
             continues and eventually you stumble upon a booth with a plaque
@@ -153,7 +233,8 @@ class walkPage extends Component {
             <button onClick={() => this.props.goFunction(seatPage)}>
               Sit down
             </button>
-          </div>
+            </div>
+            </div>
         </div>
       );
     }
@@ -198,6 +279,7 @@ class seatPage extends Component {
   render() {
     return (
       <div className="page">
+        <div className="play-area">
         <p className="story">
           As you take your seat, you notice the spectacular weather out of the
           window. Looking down at your phone it says:
@@ -218,7 +300,8 @@ class seatPage extends Component {
           <button onClick={() => this.props.goFunction(walkPage)}>
             Look up
           </button>
-        </div>
+          </div>
+          </div>
       </div>
     );
   }
